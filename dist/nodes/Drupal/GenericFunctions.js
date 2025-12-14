169708f51720a670029af5a46eeb3048b89b350e
Stephen Mulvihill <smulvih2@gmail.com>
2025-12-13 19:27:38 -0800

    Revert "Add session auth as option over basic auth"
    This reverts commit 5103dd4e89b35ad2d8719da3d4cc30a037a914bd.

--- dist/nodes/Drupal/GenericFunctions.js
+++ dist/nodes/Drupal/GenericFunctions.js
@@ -8,53 +8,8 @@ function buildJsonApiPath(resourceType, id) {
     const base = `/jsonapi/${entityTypeId}/${bundle}`;
     return id ? `${base}/${id}` : base;
 }
-const csrfTokenCache = new Map();
-function normalizeBaseUrl(input) {
-    let url = (input !== null && input !== void 0 ? input : '').trim();
-    if (!/^https?:\/\//i.test(url)) {
-        url = `https://${url}`;
-    }
-    return url.replace(/\/+$/, '');
-}
-async function ensureDrupalSessionAndCsrf(baseUrl, jar) {
-    var _a, _b, _c, _d, _e;
-    const creds = (await this.getCredentials('drupalApi'));
-    const username = (_c = (_b = (_a = creds.username) !== null && _a !== void 0 ? _a : creds.user) !== null && _b !== void 0 ? _b : creds.email) !== null && _c !== void 0 ? _c : '';
-    const password = (_e = (_d = creds.password) !== null && _d !== void 0 ? _d : creds.pass) !== null && _e !== void 0 ? _e : '';
-    if (!username || !password) {
-        throw new Error('Session auth requires username/password in the Drupal API credentials.');
-    }
-    const cacheKey = `${baseUrl}::${username}`;
-    const cached = csrfTokenCache.get(cacheKey);
-    if (cached)
-        return cached;
-    await this.helpers.httpRequest.call(this, {
-        method: 'POST',
-        url: `${baseUrl}/user/login?_format=json`,
-        json: true,
-        body: { name: username, pass: password },
-        headers: {
-            Accept: 'application/json',
-            'Content-Type': 'application/json',
-        },
-        jar,
-    });
-    const token = await this.helpers.httpRequest.call(this, {
-        method: 'GET',
-        url: `${baseUrl}/session/token`,
-        json: false,
-        jar,
-    });
-    const csrf = (typeof token === 'string' ? token : String(token)).trim();
-    csrfTokenCache.set(cacheKey, csrf);
-    return csrf;
-}
 async function drupalApiRequest(method, path, body = {}, qs = {}, options = {}) {
-    var _a, _b, _c;
-    const creds = (await this.getCredentials('drupalApi'));
-    const baseUrl = normalizeBaseUrl(String((_a = creds.baseUrl) !== null && _a !== void 0 ? _a : ''));
-    const allowUnauthorized = Boolean(creds.allowUnauthorized);
-    const authMethod = String((_b = creds.authMethod) !== null && _b !== void 0 ? _b : 'basic');
+    const { baseUrl, allowUnauthorized } = (await this.getCredentials('drupalApi'));
     const requestOptions = {
         method,
         url: `${baseUrl}${path}`,
@@ -68,15 +23,6 @@ async function drupalApiRequest(method, path, body = {}, qs = {}, options = {}) 
         rejectUnauthorized: !allowUnauthorized,
     };
     try {
-        if (authMethod === 'session') {
-            const jar = ((_c = this.helpers.request) === null || _c === void 0 ? void 0 : _c.jar) ? this.helpers.request.jar() : true;
-            if (!['GET', 'HEAD', 'OPTIONS'].includes(String(method).toUpperCase())) {
-                const csrf = await ensureDrupalSessionAndCsrf.call(this, baseUrl, jar);
-                requestOptions.headers['X-CSRF-Token'] = csrf;
-            }
-            requestOptions.jar = jar;
-            return await this.helpers.httpRequest.call(this, { ...requestOptions, ...options });
-        }
         return await this.helpers.httpRequestWithAuthentication.call(this, 'drupalApi', { ...requestOptions, ...options });
     }
     catch (error) {
